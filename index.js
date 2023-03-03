bf47dd54d5acae999c43d0ae2a6722c63fcc4aa4
Laia Mia Perez <laiperez1020@gmail.com>
2023-03-03 02:30:35 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+// function enviar (){
+
+//     // setTimeout(() => {
+        
+    
+//     // }, 5000)
+
+
+// }
+// enviar()
+// document.getElementById('submit').addEventListener('click', () => {
+//  location.reload()
+// })
