7866b630c272d542dabd981edb1d4b75891b09bb
Laia Mia Perez <laiperez1020@gmail.com>
2023-04-27 20:04:22 -0300

    ss

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
+let nombre = 'friend'
 
+window.addEventListener('load', async () => {
 
-// function enviar (){
+    const { value: password } = await Swal.fire({
+        title: 'ingresa tu nombre/apodo',
+        input: 'text',
+        inputLabel: '(no mas de 6 letras porfavor)',
+        inputPlaceholder: 'nombre',
+        inputAttributes: {
+          maxlength: 6,
+          autocapitalize: 'off',
+          autocorrect: 'off'
+        }
+      })
+      nombre = password
 
-//     // setTimeout(() => {
-        
-    
-//     // }, 5000)
+      let usuario = document.getElementById('nombre_usuario')
+        usuario.innerText = ` ${nombre} `
+})
 
-
-// }
-// enviar()
-// document.getElementById('submit').addEventListener('click', () => {
-//  location.reload()
-// })
